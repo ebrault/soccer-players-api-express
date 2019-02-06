d169b0ff0214a91630729f64def6fc94eac6c1c1
ebrault <emmet.brault@gmail.com>
2019-02-06 16:46:52 -0500

    Privatized Leagues resource
    - Added passport to league routes
    - Added requireOwnership to league routes
      - Called customErrors requireOwnership method to throw errors in
        methods that require ownership
    - Added requireToken to league routes
      - Called passport's authenticate method to require bearer based
        authentication when requireToken is called
    - Added requireToken method to each API call, i.e. all API calls require
      user to be logged in
    - Added ownership to create using req.body.league.owner = req.user.id,
      i.e. when post is called pass the user ID along in the request body
    - Added requireOwnership to Update and Delete, i.e. in order to Update
      and Delete resources, they must belong to the user
    - Duplicated players with auth CURL scripts and replaced all instances
      of players with leagues
    - Tested all league scripts with auth successfully

--- app/models/league.js
+++ app/models/league.js
@@ -8,6 +8,11 @@ const leagueSchema = new mongoose.Schema({
   country: {
     type: String,
     required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
   timestamps: true
--- app/routes/league_routes.js
+++ app/routes/league_routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const passport = require('passport')
 const League = require('../models/league')
 const handle = require('../../lib/error_handler')
 const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
-router.get('/leagues', (req, res) => {
+router.get('/leagues', requireToken, (req, res) => {
   League.find()
     .then(leagues => {
       return leagues.map(league => league.toObject())
@@ -14,23 +17,26 @@ router.get('/leagues', (req, res) => {
     .catch(err => handle(err, res))
 })
 
-router.get('/leagues/:id', (req, res) => {
+router.get('/leagues/:id', requireToken, (req, res) => {
   League.findById(req.params.id)
     .then(handle404)
     .then(league => res.status(200).json({ league: league.toObject() }))
     .catch(err => handle(err, res))
 })
 
-router.post('/leagues', (req, res) => {
+router.post('/leagues', requireToken, (req, res) => {
+  req.body.league.owner = req.user.id
   League.create(req.body.league)
     .then(league => res.status(201).json({ league: league.toObject() }))
     .catch(err => handle(err, res))
 })
 
-router.patch('/leagues/:id', (req, res) => {
+router.patch('/leagues/:id', requireToken, (req, res) => {
+  delete req.body.league.owner
   League.findById(req.params.id)
     .then(handle404)
     .then(league => {
+      requireOwnership(req, league)
       Object.keys(req.body.league).forEach(key => {
         if (req.body.league[key] === '') {
           delete req.body.league[key]
@@ -42,10 +48,13 @@ router.patch('/leagues/:id', (req, res) => {
     .catch(err => handle(err, res))
 })
 
-router.delete('/leagues/:id', (req, res) => {
+router.delete('/leagues/:id', requireToken, (req, res) => {
   League.findById(req.params.id)
     .then(handle404)
-    .then(league => league.remove())
+    .then(league => {
+      requireOwnership(req, league)
+      league.remove()
+    })
     .then(() => res.sendStatus(204))
     .catch(err => handle(err, res))
 })
