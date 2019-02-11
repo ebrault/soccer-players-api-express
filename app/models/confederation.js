b5b4e645dff853a91366e63633c67ad14aabafc9
ebrault <emmet.brault@gmail.com>
2019-02-11 16:34:22 -0500

    Privatized fifth resource: Confederations
    - Updated confederation model to enable user ownership over resource
      - owner object data structure:
        - type: objectId
        - ref: user
        - required: true
    - Updated confederation routes:
      - Added passport
      - Added requireOwnership so that server throws requireOwneship errors
        when unauthorized requests are attempted
        - requireOwnership is set to the call of the requireOwneship
          method on customErrors
      - Added requireToken for bearer based authentication
        - requireToken is set to the call of the authenticate method on
          passport with the bearer based authentication parameter passed
      - Added requireToken to all confederation route methods
      - Added req.body.confederation.owner = req.user.id to create
        - When resources are created, the owner as defined for the model is
          set to the user id
      - Added requireOwnership to update and delete, i.e. the only user who
        can update and delete their resources is them
      - Duplicated countries_auth CURL scripts as confederations_auth
        - replaced all instances of country/countries with confederation/
          confederations and appropriately changed the data object
      - Tested all confederations_auth CURL scripts successfully

--- app/models/confederation.js
+++ app/models/confederation.js
@@ -4,6 +4,11 @@ const confederationSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
   timestamps: true
--- app/routes/confederation_routes.js
+++ app/routes/confederation_routes.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const passport = require('passport')
 const Confederation = require('../models/confederation')
 const handle = require('../../lib/error_handler')
 const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
+const requireOwnership = customErrors.requireOwnership
+const requireToken = passport.authenticate('bearer', { session: false })
 const router = express.Router()
 
-router.get('/confederations', (req, res) => {
+router.get('/confederations', requireToken, (req, res) => {
   Confederation.find()
     .then(confederations => {
       return confederations.map(confederation => confederation.toObject())
@@ -14,23 +17,26 @@ router.get('/confederations', (req, res) => {
     .catch(err => handle(err, res))
 })
 
-router.get('/confederations/:id', (req, res) => {
+router.get('/confederations/:id', requireToken, (req, res) => {
   Confederation.findById(req.params.id)
     .then(handle404)
     .then(confederation => res.status(200).json({ confederation: confederation.toObject() }))
     .catch(err => handle(err, res))
 })
 
-router.post('/confederations', (req, res) => {
+router.post('/confederations', requireToken, (req, res) => {
+  req.body.confederation.owner = req.user.id
   Confederation.create(req.body.confederation)
     .then(confederation => res.status(201).json({ confederation: confederation.toObject() }))
     .catch(err => handle(err, res))
 })
 
-router.patch('/confederations/:id', (req, res) => {
+router.patch('/confederations/:id', requireToken, (req, res) => {
+  delete req.body.confederation.owner
   Confederation.findById(req.params.id)
     .then(handle404)
     .then(confederation => {
+      requireOwnership(req, confederation)
       Object.keys(req.body.confederation).forEach(key => {
         if (req.body.confederation[key] === '') {
           delete req.body.confederation[key]
@@ -42,10 +48,13 @@ router.patch('/confederations/:id', (req, res) => {
     .catch(err => handle(err, res))
 })
 
-router.delete('/confederations/:id', (req, res) => {
+router.delete('/confederations/:id', requireToken, (req, res) => {
   Confederation.findById(req.params.id)
     .then(handle404)
-    .then(confederation => confederation.remove())
+    .then(confederation => {
+      requireOwnership(req, confederation)
+      confederation.remove()
+    })
     .then(() => res.sendStatus(204))
     .catch(err => handle(err, res))
 })
